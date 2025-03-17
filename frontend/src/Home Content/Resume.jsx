127d53c4508f34d7d079df4d9ad0709da2badb85
vredchasing <therealvredchasing@gmail.com>
2025-03-17 11:15:57 -0400

    more layout changes

--- frontend/src/Home Content/Resume.jsx
+++ frontend/src/Home Content/Resume.jsx
@@ -6,18 +6,33 @@ function Resume (){
             <section className="resume-section">
                 <div className="resume-main-content">
                     <div className="resume-left">
-                        <p className="resume-left-text">Brand Design</p>
-                        <p className="resume-left-text">Operations Automation</p>
-                        <p className="resume-left-text">SEO Optimization</p>
+                        <div className="resume-img-container">
+                            <img className="resume-img" src="resume-img3.jpg"></img>
+                        </div>
+                        <div className="resume-inner">
+                            <p className="resume-text">- Brand Design</p>
+                            <p className="resume-text">- Operations Automation</p>
+                            <p className="resume-text">- SEO Optimization</p>
+                        </div>
                     </div>
                     <div className="resume-center">
-                        <p className="resume-center-text">Full-stack Development</p>
-                        <p className="resume-center-text">API/Database Expertise</p>
-                        <p className="resume-center-text">Data Science</p>
+                        <div className="resume-img-container">
+                            <img className="resume-img" src="resume-img3.jpg"></img>
+                        </div>
+                        <div className="resume-inner">
+                            <p className="resume-text">- Full-stack Development</p>
+                            <p className="resume-text">- API/Database Expertise</p>
+                            <p className="resume-text">- Data Science</p>
+                        </div>
                     </div>
                     <div className="resume-right">
-                        <p className="resume-right-text">UI/UX Design</p>
-                        <p className="resume-right-text">Graphic Design</p>
+                        <div className="resume-img-container">
+                            <img className="resume-img" src="resume-img3.jpg"></img>
+                        </div>
+                        <div className="resume-inner">
+                            <p className="resume-text">- UI/UX Design</p>
+                            <p className="resume-text">- Graphic Design</p>
+                        </div>
                     </div>
                 </div>
                 <div className="expertise-text-wrapper">
